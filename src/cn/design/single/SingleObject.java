51d17235e4875f173ddca7fbab2dbfe863403303
QinMY <15738826204@163.com>
2020-07-09 23:09:47 +0800

    添加新的分支

--- src/cn/design/single/SingleObject.java
+++ src/cn/design/single/SingleObject.java
@@ -1,8 +1,17 @@
 package cn.design.single;
 
 public class SingleObject {
-    public void say(){
-        System.out.print("Hello Design");
+	//创建私有构造方法
+	private SingleObject(){}
+	//创建一个静态实例
+	private static SingleObject instance = new SingleObject();
+	public static SingleObject getInstance(){
+		return instance;
+	}
+	public void showMessage(){
+		system.out.print("Single Pattern");
+	}
+	
     }
 
 
